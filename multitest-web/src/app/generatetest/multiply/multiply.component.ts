e591be2239dc64113347e4f44731f2af4ff210c5
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2018-03-27 20:06:01 -0400

    updates for basics multiply form

--- multitest-web/src/app/generatetest/multiply/multiply.component.ts
+++ multitest-web/src/app/generatetest/multiply/multiply.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'multiply',
   templateUrl: './multiply.component.html'
 })
 export class MultiplyComponent{
+  multiplyForm = new FormGroup ({
+    'multiple': new FormControl('',[Validators.required]),
+    'totalQuestions': new FormControl('',[Validators.required]),
+  });
+
+  isError: boolean = false;
+  errorMessage: string = '';
+
+  constructor(private router: Router){
+  }
+
+  getMultiplySet(){
+    if(this.multiplyForm.valid){
+      console.log("got Values",this.multiplyForm.get('multiple').value,)
+    }
+  }
+
 }
--- multitest-web/src/app/user/user.login.component.ts
+++ multitest-web/src/app/user/user.login.component.ts
@@ -1,5 +1,5 @@
-import { Component , OnInit} from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { UserService } from './user.service'
@@ -7,32 +7,23 @@ import { UserService } from './user.service'
 
 @Component({
   selector: 'login',
-  templateUrl: './user.login.component.html',
-  styleUrls: ['./user.login.component.css'],
-
+  templateUrl: './user.login.component.html'
 })
-export class UserLoginComponent implements  OnInit {
+export class UserLoginComponent {
 
-  loginForm: FormGroup;
-  userName: FormControl;
+  loginForm = new FormGroup ({
+    'userName': new FormControl('',[Validators.required, Validators.minLength(3)])
+  });
+
   isError: boolean = false;
-  errorMessage: string = undefined;
+  errorMessage: string = '';
 
   constructor(private router: Router, private userService: UserService){
   }
 
-  ngOnInit(): void {
-    this.isError = false;
-    this.errorMessage = undefined;
-    this.userName     = new FormControl("",Validators.required);
-    this.loginForm = new FormGroup({
-      userName: this.userName,
-    });
-  }
-
-  doLogin(formValues){
-    if(this.userName.touched && this.loginForm.valid){
-      this.userService.login(this.userName.value,"","");
+  doLogin(){
+    if(this.loginForm.valid){
+      this.userService.login(this.loginForm.get('userName').value,"","");
       this.router.navigate(['/home']);
     }
   }
