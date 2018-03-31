54f3e815fda38a48c214f01756d685fb5aa404b2
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2018-03-31 10:46:31 -0400

    updates to error message

--- multitest-web/src/app/generatetest/multiply/multiply.component.ts
+++ multitest-web/src/app/generatetest/multiply/multiply.component.ts
@@ -4,6 +4,8 @@ import { Router } from "@angular/router";
 
 import { BasicsService } from "../basics.service";
 
+import { HttpErrorResponse  } from '@angular/common/http';
+
 @Component({
   selector: "multiply",
   templateUrl: "./multiply.component.html"
@@ -22,20 +24,46 @@ export class MultiplyComponent{
   });
 
   constructor(private router: Router, private service: BasicsService){
+    this.isError = false;
+    this.errorMessage = "";
   }
 
   resetForm(){
     this.multiplyForm.get(this.CHALLENGE_NUMBER).reset();
     this.multiplyForm.get(this.TOTAL_QUESTIONS).reset();
+    this.isError = false;
+    this.errorMessage = "";
   }
 
   getMultiplySet(){
-    console.log("Challenge Number",this.multiplyForm.get(this.CHALLENGE_NUMBER).value);
-    console.log("Total Questions ",this.multiplyForm.get(this.TOTAL_QUESTIONS).value);
+    this.isError = false;
+    this.errorMessage = "";
     this.service.getMultiplyQuestions(this.multiplyForm.get(this.CHALLENGE_NUMBER).value,
         this.multiplyForm.get(this.TOTAL_QUESTIONS).value).subscribe(
-            data => { console.log(data);},
-            err  => { console.log(err);}
+            data => {
+              console.log(data);
+            },
+            err  => {
+                this.evaluateError(err);
+            }
          );
+  }
+
+  private evaluateError(err: HttpErrorResponse ){
+    console.log(err);
+    switch(err.status){
+      case 0:
+           this.errorMessage = "Cannot connect to service, please try again later";
+           this.isError = true;
+           break;
+      default:
+           if(err.error){
+             this.isError = true;
+             this.errorMessage = err.error.errorMessage;
+           }else{
+              this.errorMessage = "Internal server error from service, please try again later";
+             this.isError = true;
+           }
+    }
   }
 }
