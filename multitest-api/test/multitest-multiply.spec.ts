3c44bd2ba049c184e613963b02256818e872306b
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2018-04-09 15:08:39 -0400

    updates for test cases

--- multitest-api/test/multitest-multiply.spec.ts
+++ multitest-api/test/multitest-multiply.spec.ts
@@ -16,4 +16,23 @@ describe("multitest-multiply-impl", () => {
           });
     });
 
+    it("validateQuestion", () => {
+        const multiTest = new MultiTestMultiply();
+        const multiTestQuestions: IMultiTestQuestion[] = multiTest.generateQuestions(2, 10);
+        expect(multiTestQuestions.length).equal(10);
+        multiTestQuestions.forEach((question) => {
+          question.userAnswer = (question.number1 * question.number2);
+        });
+        multiTestQuestions.forEach((question) => {
+              expect(multiTest.validateQuestion(question)).equal(true);
+        });
+        const multiTestQuestionsWrong: IMultiTestQuestion[] = multiTest.generateQuestions(2, 10);
+        expect(multiTestQuestionsWrong.length).equal(10);
+        multiTestQuestionsWrong.forEach((question) => {
+          question.userAnswer = 1;
+        });
+        multiTestQuestionsWrong.forEach((question) => {
+              expect(multiTest.validateQuestion(question)).equal(false);
+        });
+    });
 });
