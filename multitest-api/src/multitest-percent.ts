05798259d058fe4f28456a28fdcc43552195ee6b
malcolm.j.pereira <malcolm.j.pereira@gmail.com>
2018-04-10 16:40:38 -0400

    updates for percent

--- multitest-api/src/multitest-percent.ts
+++ multitest-api/src/multitest-percent.ts
@@ -8,12 +8,21 @@ export class MultiTestPercent  extends MultiTestBase implements IMultiTestType {
 
     generateQuestions(cNumber: number, tQuestions: number):IMultiTestQuestion[] {
         let jumpSet:number  = (tQuestions > 10) ? 3 : 5;
-        const questionArr:IMultiTestQuestion[] = this.generateSimpleSet(jumpSet);
+        let questionArr:IMultiTestQuestion[] = this.generateSimpleSet(jumpSet);
+        questionArr = questionArr.concat(this.generateBasicSet(tQuestions - questionArr.length));
         return questionArr;
     }
 
     validateQuestion(question: IMultiTestQuestion) : boolean {
-        return false;
+        if (question.userAnswer === (question.number1 * question.number2 / 100) ) {
+            question.correct = true;
+            question.correctAnswer = (question.number1 * question.number2 / 100);
+            return true;
+          }else{
+            question.correct = false;
+            question.correctAnswer = (question.number1 * question.number2 / 100);
+            return false;
+          }
     }
 
     private generateSimpleSet(simpleSetTotal: number):IMultiTestQuestion[]  {
@@ -38,5 +47,46 @@ export class MultiTestPercent  extends MultiTestBase implements IMultiTestType {
             }
         }
         return questionArr;
+    }
+
+    private generateBasicSet(simpleSetTotal: number):IMultiTestQuestion[]  {
+        const questionArr:IMultiTestQuestion[] = [];
+        const numberGeneratedList:number[] = [];
+
+        let counter:number = simpleSetTotal;
+        let nextSet = this.getBasicSet(0);
+        let min  = nextSet[0][0];
+        let max  = nextSet[0][1];
+
+        let setCounter:number  = 1;
+        let basicSetCounter:number  = 1;
+        let jumpSet:number = 5;
+
+        while (counter >= 1) {
+            if(setCounter >  jumpSet ) {
+              setCounter = 1;
+              basicSetCounter++;
+              nextSet = this.getBasicSet(basicSetCounter);
+              min  = nextSet[0][0];
+              max  = nextSet[0][1];
+            }
+            const baseNumberGenerated:number =  Math.floor(Math.random() * (99 - 2)) + 2;
+            var numberGenerated:number = Math.floor(Math.random() * ((Math.floor(Math.random() * (max - min + 1)) + (Math.floor(Math.random() * (max - min + 1)) + min)) - (Math.floor(Math.random() * (max - min + 1)) + min) + 1)) + (Math.floor(Math.random() * (max - min + 1)) + min);
+            if (numberGeneratedList.indexOf(numberGenerated) === -1) {
+                  questionArr.push(this.getMultiTestQuestion(baseNumberGenerated,numberGenerated));
+                  numberGeneratedList.push(numberGenerated);
+                  counter--;
+                  setCounter++;
+            }
+        }
+
+        return questionArr;
+    }
+
+    private getBasicSet(max):number[][] {
+         if(max > 0){
+            return [[(max)*100,(max*2)*89]];
+          }
+          return [[1*100,200]];
     }
 }
--- multitest-api/test/multitest-percent.spec.ts
+++ multitest-api/test/multitest-percent.spec.ts
@@ -9,13 +9,33 @@ describe("multitest-percent", () => {
   it("generateQuestions", () => {
         const multiTest = new MultiTestPercent();
         const multiTestQuestions: IMultiTestQuestion[] = multiTest.generateQuestions(0, 10);
-        expect(multiTestQuestions.length).equal(5);
-        console.log(multiTestQuestions);
+        expect(multiTestQuestions.length).equal(10);
         multiTestQuestions.forEach((question) => {
-          expect(question.number1).to.be.above(0);
-          expect(question.number2).to.be.above(0);
+          expect(question.number1).to.be.above(1);
+          expect(question.number2).to.be.above(100);
         });
   });
 
+  it("validateQuestion", () => {
+        const multiTest = new MultiTestPercent();
+        const multiTestQuestions: IMultiTestQuestion[] = multiTest.generateQuestions(0, 10);
+        expect(multiTestQuestions.length).equal(10);
+        multiTestQuestions.forEach((question) => {
+          expect(question.number1).to.be.above(1);
+          expect(question.number2).to.be.above(100);
+          question.userAnswer = (question.number1 * question.number2 / 100);
+        });
+        multiTestQuestions.forEach((question) => {
+             expect(multiTest.validateQuestion(question)).equal(true);
+        });
+        const multiTestQuestionsWrong: IMultiTestQuestion[] = multiTest.generateQuestions(0, 10);
+        expect(multiTestQuestionsWrong.length).equal(10);
+        multiTestQuestionsWrong.forEach((question) => {
+          question.userAnswer = -1;
+        });
+        multiTestQuestionsWrong.forEach((question) => {
+             expect(multiTest.validateQuestion(question)).equal(false);
+        });
+  });
 
 });
